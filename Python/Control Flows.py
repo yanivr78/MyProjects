2e424955d8768a8064980ea932d37810a46f80bd
yaniv <yanivr78@gmail.com>
2022-06-04 23:16:12 -0500

    Added more context to Control_Flows.py

--- Python/Control Flows.py
+++ Python/Control Flows.py
@@ -8,21 +8,32 @@ print("Welcome to the roller coaster!")
 # height == 11 - Two Equal signs means "Check Equality"
 
 height = int(input("What is your height in cm?"))
-age = int(input("What is your age?"))
+bill = 0
+
 if height > 120:
     print("You can ride the roller-coaster")
+    age = int(input("What is your age?"))
     if age < 12:
+        bill = 5
         print("Please pay $5.")
     elif age <= 18:
+        bill = 7
         print("Please pay $7.")
     else:
+        bill = 12
         print("Please pay $12")
+    wants_photo = input("Do you  want a photo taken ? Y or N")
+    if wants_photo == "Y" or "y":
+        bill += 3
+
+    print(f"Your final bill is {bill}")
+
 else:
     print("You are too short  to ride the roller coaster")
 
 
 # Modulo % Gives you the reminder , for example:
-7 % 2 # How many times does 2 fits in 7 ? - 2 + 2 + 2 + 1 << the remainder is one
+# 7 % 2 means How many times does 2 fits in 7 ? - 2 + 2 + 2 + 1 << the remainder is one
 
 # Understanding if a number is odd or even
 number = int(input("Which number do you want to check?"))
