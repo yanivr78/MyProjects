62c69a75001d67a31c6b27941b9dd897d4d5979d
yaniv <yanivr78@gmail.com>
2022-06-06 21:20:41 -0500

    Added Nested_lists.py

---
+++ Python/Nested_lists.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+
+fruits = ["Streberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatos", "Celery", "Potatoes"]
+dirty_dosen = [fruits, vegetables]
+print(dirty_dosen)
+print(dirty_dosen[1][1])
+print(dirty_dosen[0][1])
