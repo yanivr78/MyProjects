aa3c78af9960cf00db9926edc5d25343277305dc
yaniv <yanivr78@gmail.com>
2022-09-08 22:07:30 -0500

    Renamed terraforming/ to Terraform/, removed all course and added a new course for Terraform + EKS (https://github.com/stacksimplify/terraform-on-aws-eks)

---
+++ Python/Cipher.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def greet(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like to be in {location}?")
+    print("Isn't the weather nice today?")
+
+
+greet("Yaniv", "Texas")
